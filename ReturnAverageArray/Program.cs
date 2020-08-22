84d92bd41fde6e173681c6c85e3880642f2593ab
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2020-08-22 15:40:16 +0300

    Добавьте файлы проекта.

---
+++ ReturnAverageArray/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace ReturnAverageArray {
+    class Program {
+        static int[] arr = new int[] { 1, 4, 5 };
+        static void Main() {            
+            int result = CalculateAverage(arr);
+            Console.WriteLine($"Результат: {result}");
+            Console.ReadKey();
+        }
+        static int CalculateAverage(int[] arr) => (int)arr.Average();
+    }
+}
